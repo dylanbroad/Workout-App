399c6e3b1b34746e08de63b55c3dbf3f30a58a5e
dylanbroad <87337533+dylanbroad@users.noreply.github.com>
2023-03-21 17:14:11 -0400

    Delete setUpProxy.js

--- setUpProxy.js
+++
@@ -1,5 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-module.exports = function(Login) {
-    Login.use('/users', createProxyMiddleware({ target: 'http://localhost:5000', changeOrigin: true }));
-  };
